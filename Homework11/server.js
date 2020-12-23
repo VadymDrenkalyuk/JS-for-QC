58f7eca8bfa981a754e6cd5e6fdf00431e9f7f2e
Vadym Drenkaliuk <drenkalyukv@gmail.com>
2020-12-23 19:42:34 +0200

    Added HW11

--- Homework11/server.js
+++ Homework11/server.js
@@ -6,10 +6,10 @@ server.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end(
         `System Information\n
-Current user name: ${os.userInfo().username}\n
-OS type: ${os.type()}\n
-System work time: ${os.uptime / 60} minutes\n
-Current work directory: ${__dirname}\n
-Server file name: ${path.basename(__filename)}`
+        Current user name: ${os.userInfo().username}\n
+        OS type: ${os.type()}\n
+        System work time: ${os.uptime / 60} minutes\n
+        Current work directory: ${__dirname}\n
+        Server file name: ${path.basename(__filename)}`
     );
 }).listen(5000);
